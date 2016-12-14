278d64ef8bd6c5bdbeddce365dbe97bf1bbdc60d
antciccone <aciccone@bu.edu>
2016-12-14 10:56:49 -0700

    passing feature story test

--- spec/features/finding_gas_station_by_location_spec.rb
+++ spec/features/finding_gas_station_by_location_spec.rb
@@ -6,9 +6,15 @@ describe "as s user" do
 
       visit "/"
 
-      fill_in :q, with: '07871'
+      fill_in :q, with: '80203'
       click_button "Locate"
+
       expect(current_path).to eq(search_path)
+      expect(page).to have_content("Name")
+      expect(page).to have_content("Address")
+      expect(page).to have_content("Fuel Type")
+      expect(page).to have_content("Distance")
+      expect(page).to have_content("Hours")
     end
   end
 end
