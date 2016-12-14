691c963244c18dd0c05380674933beaa33a48a55
antciccone <aciccone@bu.edu>
2016-12-14 09:48:09 -0700

    adding user story

--- app/services/nrel_service.rb
+++ app/services/nrel_service.rb
@@ -6,7 +6,7 @@ class NrelService
   end
 
   def by_address(address)
-    by_address_json = Faraday.get('http://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key=#{token}&location=#{address}')
-    require "pry"; binding.pry
+    by_address_json = Faraday.get("http://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key=#{token}&location=#{address}")
+    JSON.parse(by_address_json.body, symbolize_names: true)[:fuel_stations]
   end
 end
---
+++ spec/features/finding_gas_station_by_location_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe "as s user" do
+  context "when i visit root" do
+    scenario "a fill in a form by address" do
+
+      visit "/"
+
+      fill_in 
+
+    end
+  end
+end
