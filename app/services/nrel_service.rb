8398c685c43df9af2e9b93696df49fd848368ec4
antciccone <aciccone@bu.edu>
2016-12-14 11:16:33 -0700

    final commit

--- app/services/nrel_service.rb
+++ app/services/nrel_service.rb
@@ -1,12 +1,20 @@
 class NrelService
 
-  attr_reader :token
   def initialize(token)
     @token = token
+    @url = "http://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?"
   end
 
   def by_address(address)
-    by_address_json = Faraday.get("http://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key=#{token}&location=#{address}")
-    JSON.parse(by_address_json.body, symbolize_names: true)[:fuel_stations]
+    by_address_json = Faraday.get("#{url}api_key=#{token}&location=#{address}")
+    json_parse(by_address_json)
   end
+
+  def json_parse(json)
+    JSON.parse(json.body, symbolize_names: true)[:fuel_stations]
+  end
+
+  private
+
+    attr_reader :token, :url
 end
