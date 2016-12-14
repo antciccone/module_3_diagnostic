f89fc8c85ef895a17da1309f7c5acfbe6842ad42
antciccone <aciccone@bu.edu>
2016-12-14 10:46:54 -0700

    building out the view

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,6 @@
 class SearchController < ApplicationController
 
   def index
-    @by_address = NrelAddress.address(params[:q])
+    @by_address = NrelAddress.by_address(params[:q], ENV['API_KEY'])
   end
 end
